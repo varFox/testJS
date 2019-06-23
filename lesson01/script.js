5915f5deafe83ee4566816eda75f279b06545f7f
Lyubov <aira_dark@live.ru>
2019-06-24 02:23:31 +0300

    lesson02

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,3 +1,17 @@
-let money, income, addExpenses, deposit, mission, period;
-alert('dsf');
-console.log('asd');
+let money = 58000,
+    income = 5000,
+    addExpenses = 'счета по квартире, обучение, пицца',
+    deposit = true,
+    mission = 50000,
+    period = 6;
+
+console.log(money);
+console.log(income);
+console.log(deposit);
+console.log(income.toString().length);
+console.log(`Период ${period} месяцев. Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+
+console.log(`Дневной бюджет: ${budgetDay.toFixed()}. Остаток: ${money % 30}`);
